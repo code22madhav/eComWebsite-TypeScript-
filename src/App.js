cfd55aa83b1a37fde021810f18d7d4ad8ee4fc60
Madhav Pathak <73849357+code22madhav@users.noreply.github.com>
2024-12-06 20:09:39 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+
+
 
 function App (){  
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path="shop" element={"SHOP page"}/>
+      </Route>
     </Routes>
   )
 }
